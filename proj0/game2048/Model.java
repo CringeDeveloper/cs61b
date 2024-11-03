f3ba8cdbaa6e35584c9f3306b5baa87e14d0902b
Vladislav <warzon131@gmail.com>
2024-11-03 23:25:43 +0200

    add atLeastOneMoveExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -176,6 +176,23 @@ public class Model extends Observable {
             return true;
         }
 
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if(i != 0 && b.tile(i, j).value() == b.tile(i - 1, j).value()) {
+                    return true;
+                }
+                if (i != b.size() - 1 && b.tile(i, j).value() == b.tile(i + 1, j).value()) {
+                    return true;
+                }
+                if (j != 0 && b.tile(i, j).value() == b.tile(i, j - 1).value()) {
+                    return true;
+                }
+                if (j != b.size() - 1 && b.tile(i, j).value() == b.tile(i, j + 1).value()) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 
