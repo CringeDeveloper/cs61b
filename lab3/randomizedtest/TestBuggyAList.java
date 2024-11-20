cb48130a4711d039ff1997b22e17b5e180245e56
Vladislav <warzon131@gmail.com>
2024-11-20 22:02:09 +0200

    Simple Comparison Test

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -8,5 +8,23 @@ import static org.junit.Assert.*;
  * Created by hug.
  */
 public class TestBuggyAList {
-  // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correct = new AListNoResizing<Integer>();
+        BuggyAList<Integer> buggy = new BuggyAList<Integer>();
+
+        correct.addLast(1);
+        correct.addLast(2);
+        correct.addLast(3);
+
+        buggy.addLast(1);
+        buggy.addLast(2);
+        buggy.addLast(3);
+
+        assertEquals(correct.size(), buggy.size());
+
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+        assertEquals(correct.removeLast(), buggy.removeLast());
+    }
 }
