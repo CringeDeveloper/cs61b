8285e37e24072f087ccf3fe62d65d52f38949aa8
Vladislav <warzon131@gmail.com>
2024-11-24 00:42:32 +0200

    last first works

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,5 +1,67 @@
 package deque;
 
 public class LinkedListDeque {
+    static class Node {
+        public int item = 0;
+        public Node next = null;
+        public Node prev = null;
 
+        public Node(int i, Node p, Node n) {
+            prev = p;
+            next = n;
+            item = i;
+        }
+    }
+
+    private Node sentinelFront;
+    private Node sentinelBack;
+    private int size;
+
+//    public LinkedListDeque(int x) {
+//        sentinel = new Node(42, null, null);
+//        sentinel.next = new Node(x, null, null);
+//
+//        size = 1;
+//    }
+    public LinkedListDeque() {
+        sentinelFront = new Node(42, null, null);
+        sentinelBack = new Node(42, null, null);
+        sentinelFront.next = sentinelBack;
+        sentinelBack.prev = sentinelFront;
+
+        size = 0;
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printDeque() {
+        Node temp = sentinelFront.next;
+
+        for(int i = 0; i < size; i++) {
+            System.out.print(temp.item + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public void addFirst(int x) {
+        sentinelFront.next = new Node(x, sentinelFront, sentinelFront.next);
+        sentinelFront.next.next.prev = sentinelFront.next;
+        size += 1;
+    }
+
+    public void addLast(int x) {
+        sentinelBack.prev = new Node(x, sentinelBack.prev, sentinelBack);
+        sentinelBack.prev.prev.next = sentinelBack.prev;
+
+        size += 1;
+    }
 }
